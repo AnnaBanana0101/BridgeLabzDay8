041d3fff9a170b2f8e99decad9a4cad1ce763fc3
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-26 20:35:57 +0530

    Implemented UC4

--- GenericMax.java
+++ GenericMax.java
@@ -1,27 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class GenericMax<T extends Comparable<T>>{
 
-    T A;
-    T B;
-    T C;
+    // T A;
+    // T B;
+    // T C;
 
-    public GenericMax(T a, T b, T c)
-    {
-        A = a;
-        B = b;
-        C = c;
-    }
+    // public GenericMax(T a, T b, T c)
+    // {
+    //     A = a;
+    //     B = b;
+    //     C = c;
+    // }
 
 
-    public T findMax()
+    public Optional<T> findMax(T...values)
     {
         //return A.compareTo(B)>=0 ?( B.compareTo(C)>=0 ? A : C ): A.compareTo(C) >=0? B : B.compareTo(C)>=0? B: C;
 
-        if(A.compareTo(B)>=0 && A.compareTo(C)>=0)
-            return (T) A;
-        else if(A.compareTo(B)<=0 && B.compareTo(C)>=0)
-            return (T) B;
-        else
-            return (T) C;
+        // if(A.compareTo(B)>=0 && A.compareTo(C)>=0)
+        //     return (T) A;
+        // else if(A.compareTo(B)<=0 && B.compareTo(C)>=0)
+        //     return (T) B;
+        // else
+        //     return (T) C;
+
+        Arrays.sort(values, Comparator.reverseOrder());
+        return Optional.of(values[0]);
     }
 }
--- MaxMain.java
+++ MaxMain.java
@@ -1,46 +1,46 @@
 import java.util.*;
+import java.util.Optional;
 
 public class MaxMain {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Enter the three Strings: ");
+        // System.out.println("Enter the three Strings: ");
 
-        System.out.println("a:");
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
+        // System.out.println("a:");
+        // Scanner sc = new Scanner(System.in);
+        // String a = sc.nextLine();
 
-        System.out.println("b:");
-        String b = sc.nextLine();
+        // System.out.println("b:");
+        // String b = sc.nextLine();
 
-        System.out.println("c:");
-        String c = sc.nextLine();
+        // System.out.println("c:");
+        // String c = sc.nextLine();
 
-        GenericMax genericMax = new GenericMax(a,b,c);
+        GenericMax<String> genericMax = new GenericMax();
+        Optional<String> result = genericMax.findMax("apple", "banana", "peach", "orange");
 
-        String maximum = (String) genericMax.findMax();
+        //String maximum = genericMax.findMax();
 
-        System.out.println("Maximum: "+maximum);
+        System.out.println("Maximum: "+ result);
         System.out.println("");
 
-        System.out.println("Enter the three Integers: ");
+        // System.out.println("Enter the three Integers: ");
 
-        System.out.println("a:");
-        //Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
+        // System.out.println("a:");
+        // //Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();
 
-        System.out.println("b:");
-        int y = sc.nextInt();
+        // System.out.println("b:");
+        // int y = sc.nextInt();
 
-        System.out.println("c:");
-        int z = sc.nextInt();
+        // System.out.println("c:");
+        // int z = sc.nextInt();
 
-        GenericMax genericIntMax = new GenericMax(x, y, z);
-        int max = (int) genericIntMax.findMax();
-
+        GenericMax<Integer> genericIntMax = new GenericMax();
+        //int max = genericIntMax.findMax();
+        Optional<Integer> max = genericIntMax.findMax(1,2,3,4,5);
         System.out.println("Maximum: "+max);
         System.out.println("");
-
-
 
     }
 }
